8d6614804bbda00f0bdc626e818ffbf356ecb2f9
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2026-01-13 21:48:59 +0300

    Fix default choice for Actions CI/Runner installation prompt

--- action_register.sh
+++ action_register.sh
@@ -4,12 +4,10 @@ sudo chown -R 1001:1001 runner-data
 
 if [ -t 0 ]; then
   docker exec -it forgejo-runner /bin/forgejo-runner register
+elif [ -e /dev/tty ]; then
+  docker exec -i forgejo-runner /bin/forgejo-runner register < /dev/tty
 else
-  if command -v script >/dev/null 2>&1; then
-    script -qc "docker exec -it forgejo-runner /bin/forgejo-runner register" /dev/null
-  else
-    docker exec -i forgejo-runner /bin/forgejo-runner register
-  fi
+  script -qc "docker exec -it forgejo-runner /bin/forgejo-runner register" /dev/null
 fi
 
 sudo chown -R 1001:1001 runner-data
