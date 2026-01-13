56656993bd3662dbd22161dab3ba14ba24d23a2a
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2026-01-13 23:57:00 +0300

    Remove runner name input from registration script for simplicity 🧠

--- action_register.sh
+++ action_register.sh
@@ -2,14 +2,10 @@ cd ../forgejo
 sleep 2
 sudo chown -R root:root runner-data
 
-printf "Runner Name: "
-read runner_name
-[ -z "$runner_name" ] && runner_name=$(hostname)
-
 if [ -e /dev/tty ]; then
-  docker exec -it forgejo-runner /bin/forgejo-runner register --name "$runner_name" --labels "docker,$runner_name" </dev/tty >/dev/tty
+  docker exec -it forgejo-runner /bin/forgejo-runner register </dev/tty >/dev/tty
 else
-  script -qc "docker exec -it forgejo-runner /bin/forgejo-runner register --name \"$runner_name\" --labels \"docker,$runner_name\"" /dev/null
+  script -qc "docker exec -it forgejo-runner /bin/forgejo-runner register" /dev/null
 fi
 
 sudo chown -R root:root runner-data
