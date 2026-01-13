236a3d85da031ab309f0d30a8bf59c500b764c76
DeveloperKubilay <kubiss52@yahoo.com>
2026-01-13 21:14:19 +0300

    dcfv

--- action_register.sh
+++ action_register.sh
@@ -1,9 +1,16 @@
 cd ../forgejo
 sleep 2
-pwd
-sudo chown -R 1001:1001 runner-data
-docker exec -it forgejo-runner /bin/forgejo-runner register
 sudo chown -R 1001:1001 runner-data
 
+if [ -t 0 ]; then
+  docker exec -it forgejo-runner /bin/forgejo-runner register
+elif [ -e /dev/tty ]; then
+  docker exec -i forgejo-runner /bin/forgejo-runner register < /dev/tty
+else
+  script -qc "docker exec -it forgejo-runner /bin/forgejo-runner register" /dev/null
+fi
+
+sudo chown -R 1001:1001 runner-data
+docker compose restart forgejo-runner
+
 echo "Runner registration complete."
-docker compose restart server
