aaf809a557f15e46b7f1ea953e9d4e41f33d654e
DeveloperKubilay <kubiss52@yahoo.com>
2026-01-13 21:47:35 +0300

    sdasdsa

--- action_register.sh
+++ action_register.sh
@@ -5,7 +5,11 @@ sudo chown -R 1001:1001 runner-data
 if [ -t 0 ]; then
   docker exec -it forgejo-runner /bin/forgejo-runner register
 else
-  script -qc "docker exec -it forgejo-runner /bin/forgejo-runner register" /dev/null
+  if command -v script >/dev/null 2>&1; then
+    script -qc "docker exec -it forgejo-runner /bin/forgejo-runner register" /dev/null
+  else
+    docker exec -i forgejo-runner /bin/forgejo-runner register
+  fi
 fi
 
 sudo chown -R 1001:1001 runner-data
