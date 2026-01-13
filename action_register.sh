0324919200a150c7439e0892fe3a47ba23113b1f
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2026-01-13 22:19:26 +0300

    Fix ownership in runner registration script and update docker-compose to use root user

--- action_register.sh
+++ action_register.sh
@@ -1,6 +1,6 @@
 cd ../forgejo
 sleep 2
-sudo chown -R 1001:1001 runner-data
+sudo chown -R root:root runner-data
 
 if [ -e /dev/tty ]; then
   docker exec -it forgejo-runner /bin/forgejo-runner register </dev/tty >/dev/tty
@@ -8,7 +8,7 @@ else
   script -qc "docker exec -it forgejo-runner /bin/forgejo-runner register" /dev/null
 fi
 
-sudo chown -R 1001:1001 runner-data
+sudo chown -R root:root runner-data
 docker compose restart forgejo-runner
 
 echo "Runner registration complete."
